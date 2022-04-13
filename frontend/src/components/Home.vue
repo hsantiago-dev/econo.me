3e06f06a6320ee0242e113f8e4c6cb41acd09479
Henrick Santiago <ti.henrick@unimedpg.com.br>
2022-04-13 14:24:08 -0300

    Pequena correção no logout

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -111,8 +111,19 @@
 
                 await axios.post('/logout', {}, getToken())
                 .finally(() => {
+                    this.deleteAllCookies();
                     this.$router.push('/').catch(() => {})
                 })
+            },
+            deleteAllCookies() {
+                let cookies = document.cookie.split(";");
+
+                for (let i = 0; i < cookies.length; i++) {
+                    let cookie = cookies[i];
+                    let eqPos = cookie.indexOf("=");
+                    let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+                }
             }
         }
     }
